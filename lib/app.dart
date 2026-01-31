d9011f5cfb23ae3dbab9eab4fbe4330ab055339b
fahim <thebigredbhuiyan@gmail.com>
2026-01-31 23:42:59 +0600

    feat

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:esp32_controller/screens/light_controller_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const LightControlPage(),
+    );
+  }
+}
